2589ed56d74302e3e797f1683a0498c380c36692
luisacherrera <la.herrera457@gmail.com>
2020-09-19 13:12:10 +0200

    app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { ListedElementComponent } from './listed-element/listed-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    UserCardComponent
+    UserCardComponent,
+    ListedElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listed-element/listed-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listed-element',
+  templateUrl: './listed-element.component.html',
+  styleUrls: ['./listed-element.component.scss']
+})
+export class ListedElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
