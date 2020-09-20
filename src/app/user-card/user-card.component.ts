3e73c2f4baf824bf4a6544eec85d182dad6041f8
luisacherrera <la.herrera457@gmail.com>
2020-09-20 14:28:05 +0200

    basic single user page working

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserServiceService } from '../user-service.service';
+import { forkJoin } from 'rxjs';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-card',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent implements OnInit {
+  id : string;
+  user: User;
+  followers: any;
+  repositories: any;
 
-  constructor() { }
+  constructor(
+    private _userService : UserServiceService,
+    private _route : ActivatedRoute
+  ) { 
+    this.id = this._route.snapshot.params.id;
+    
+    const userObservable = forkJoin({
+      userInfo: this._userService.getSingleUser(this.id),
+      userFollowers: this._userService.getUserFollowers(this.id),
+      userRepos: this._userService.getUserRepos(this.id)
+    })
+
+    userObservable.subscribe({
+      next: (value) => {
+        this.user = new User(
+          value.userInfo.login,
+          value.userInfo.html_url,
+          value.userInfo.name,
+          value.userInfo.avatar_url,
+          value.userInfo.email,
+          value.userInfo.bio
+        );
+
+        this.followers = value.userFollowers; 
+        this.repositories = value.userRepos;
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
-
 }
